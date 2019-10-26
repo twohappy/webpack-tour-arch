beaf0c1b7f86481b265e1e4416e614b4805c817e
游天琪 <autumnyout@gmail.com>
2019-10-26 18:54:18 +0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
 import _ from 'lodash';
-import './styles/style.css';
-import Fly from './assets/fly.jpg';
-import './assets/font-awesome-4.7.0/css/font-awesome.min.css';
-import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const flyPig = new Image();
-    flyPig.src = Fly;
-    element.appendChild(flyPig);
 
     return element;
 }
-function component2() {
-    const element = document.createElement('div');
-    const fragment = require("html-loader!./fragment.html");
-
-    element.innerHTML = fragment;
-
-    return element;
-}
-function component3() {
-    const element = document.createElement('i');
-    element.classList.add('fa');
-    element.classList.add('fa-plus');
-
-    return element;
-}
-console.log(Data);
 document.body.appendChild(component());
-document.body.appendChild(component2());
-document.body.appendChild(component3());
