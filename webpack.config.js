bede85db6eea686475ba40bb08f2155c1643736a
游天琪 <autumnyout@gmail.com>
2019-10-26 19:03:37 +0800

    preparation

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [new BundleAnalyzerPlugin()]
