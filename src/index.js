8df6f09148b9544295ebfd3ab91dbeb8c63a60be
游天琪 <autumnyout@gmail.com>
2019-10-26 17:58:59 +0800

    loading image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Fly from './fly.jpg';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const flyPig = new Image();
+    flyPig.src = Fly;
+
+    element.appendChild(flyPig);
 
     return element;
 }
