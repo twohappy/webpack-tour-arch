32a0b2c13730bf71234a3c27b1c1cf78614314d0
游天琪 <autumnyout@gmail.com>
2019-10-26 18:30:54 +0800

    html-loader

--- src/index.js
+++ src/index.js
@@ -10,10 +10,18 @@ function component() {
 
     const flyPig = new Image();
     flyPig.src = Fly;
-
     element.appendChild(flyPig);
+
+    return element;
+}
+function component2() {
+    const element = document.createElement('div');
+    const fragment = require("html-loader!./fragment.html");
+
+    element.innerHTML = fragment;
 
     return element;
 }
 
 document.body.appendChild(component());
+document.body.appendChild(component2());
