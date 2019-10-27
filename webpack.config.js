ea165efecc1f0f224e0cf0fd40345bf5f32b56a7
游天琪 <autumnyout@gmail.com>
2019-10-27 15:10:08 +0800

    entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        another: './src/another-module.js',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
